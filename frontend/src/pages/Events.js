46c82906c6b0fd08c18b30c7c15a4b192091394e
hzz4343 <hzz1996@qq.com>
2023-09-03 20:26:20 -0500

    first commit

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,44 @@
+import { useLoaderData, json, defer, Await } from "react-router-dom";
+import { Suspense } from "react";
+
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const { events } = useLoaderData();
+
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>loading..</p>}>
+      <Await resolve={events}>
+        {(loadEvents) => <EventsList events={loadEvents} />}
+      </Await>
+    </Suspense>
+  );
+}
+
+export default EventsPage;
+
+async function loadEvents() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return { isError: true, message: 'Could not fetch events.' };
+    // throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+    //   status: 500,
+    // });
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
+}
